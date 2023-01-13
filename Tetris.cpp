888c4886a2ba3594bc237be7dbae776d3f08e089
artem-kondratew <artemkondratev5@gmail.com>
2023-01-13 23:43:43 +0300

    Test text works in new buffer

--- Tetris.cpp
+++ Tetris.cpp
@@ -11,21 +11,36 @@ void Tetris::init() {
     nonl();  //  deny going to the new line
     cbreak();  //  буфер передается без нажатия enter
     noecho();  //  не отображается печать символов
-    timeout(0); //  неблокируюшее чтение
-    leaveok(stdscr, TRUE);  //  сдвиг курсора - нормально
+    //timeout(0); //  неблокируюшее чтение
+    //leaveok(stdscr, TRUE);  //  сдвиг курсора - нормально
     curs_set(0);  //  спрятать курсор
     //signal(SIGINT, sighandler);  //  обработка Ctrl + C
     //signal(SIGQUIT, sighandler);  //  обработка Ctrl + обратный /
     //signal(SIGWINCH, sighandler);  //  обработка изменения размера окна
     //signal(SIGTSTP, sighandler);  //  обработка Ctrl + Z
+    printw("TEST\n");
+    refresh();
+    getch();
 }
 
 
 void Tetris::finish() {
+        //start debug
+    printw("exit\n");
+    refresh();
+    getch();
+        //end debug
     curs_set(1);  //  показать курсор
     clear();  //  очистка экрана
     refresh();
     resetty();  //  восстановление настроек терминала
-    endwin();  //  подготовка к выходу
+    endwin();
     exit(0);
 }
+
+/*
+void Tetris::finish() {
+
+
+}
+*/
--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
+#include <iostream>
 #include "Tetris.h"
 
 
 int main() {
+
+    Tetris::init();
+    //std::cout << "I'm here now";
+    Tetris::finish();
 
     return 0;
 }
